cab45fcd728446ef6f31c39e8d2bba73da41da77
Gabriel Prado <gaprados3@gmail.com>
2024-11-23 21:24:18 -0300

    feat: add users db schema and add user id to goals

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,17 +1,27 @@
 import dayjs from 'dayjs'
 import { client, db } from '.'
-import { goalCompletions, goals } from './schema'
+import { goalCompletions, goals, users } from './schema'
 
 async function seed() {
   await db.delete(goalCompletions)
   await db.delete(goals)
 
+  const [user] = await db
+    .insert(users)
+    .values({
+      name: 'John Doe',
+      email: '',
+      externalAccountId: 83674872,
+      avatarUrl: 'https://github.com/gaprados.png',
+    })
+    .returning()
+
   const result = await db
     .insert(goals)
     .values([
-      { title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
-      { title: 'Fazer exercício', desiredWeeklyFrequency: 3 },
-      { title: 'Meditar', desiredWeeklyFrequency: 1 },
+      { userId: user.id, title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
+      { userId: user.id, title: 'Fazer exercício', desiredWeeklyFrequency: 3 },
+      { userId: user.id, title: 'Meditar', desiredWeeklyFrequency: 1 },
     ])
     .returning()
 
