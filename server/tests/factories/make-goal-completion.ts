e90608687b610a1c8b2a4b207d2c313bffb1f556
Gabriel Prado <gaprados3@gmail.com>
2024-11-24 22:38:01 -0300

    test: add unit tests for services

--- server/tests/factories/make-goal-completion.ts
+++ server/tests/factories/make-goal-completion.ts
@@ -2,7 +2,7 @@ import { db } from '../../src/db'
 import { goalCompletions } from '../../src/db/schema'
 import type { InferSelectModel } from 'drizzle-orm'
 
-export async function makeGoalCompletions(
+export async function makeGoalCompletion(
   override: Partial<InferSelectModel<typeof goalCompletions>> &
     Pick<InferSelectModel<typeof goalCompletions>, 'goalId'>
 ) {
